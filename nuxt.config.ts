a21143974add702991092212981602efd5509445
Sergey Denisov <sergdenisov@ozon.ru>
2023-07-27 17:12:15 +0800

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,11 +12,14 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Volokut – комната массажных дел на набережной реки Мойки',
+      htmlAttrs: {
+        lang: 'ru'
+      },
       script: [
         {
           type: 'text/javascript',
           fetchpriority: 'low',
-          innerHTML: '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(94251690, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true })',
+          innerHTML: process.env.METRIKA_STRING,
           async: true,
         },
       ],
